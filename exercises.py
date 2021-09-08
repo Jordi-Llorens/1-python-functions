5afd07b983cab56797f395ade5196afced89ac99
Sergio-Yersi Villegas Pelegrín <89159197+syvillegas@users.noreply.github.com>
2021-09-08 19:56:21 +0200

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,4 +1,4 @@
-#
+# gpy
 # Your can take a look at the tests
 # in "test_exercises", but do not need to
 # change anything there.
@@ -46,3 +46,47 @@
 # not a string, it should return "Please provide a name."
 # i.e. 5 --> "Please provide a name."
 #
+
+# 1)
+
+def triple(x):
+    y = 3*x
+    return y
+
+#x=int(input("Value of which we want its triple: "))
+x=3
+valor1=triple(x)
+print("1r valor = ", valor1)
+
+# 2)
+
+def substract(a,b):
+    z = b-a
+    return z
+#a=int(input("First substract parameter is "))
+#b=int(input("Second substract parameter is "))
+a=2
+b=1
+valor2=substract(a,b)
+print("2o valor = ", valor2)
+
+
+# 3)
+def safe_substract(c,d):
+    try:
+        return d-c
+    except TypeError:
+        return None
+
+valor3 = safe_substract(2, '1')
+print("3r valor = ", valor3)
+
+# 4)  
+def greet_person(e):
+    try:
+        return 'Hello, ' + e + '!'
+    except TypeError:
+        return 'Please provide a name'
+
+valor4 = greet_person('Sophia')
+print(valor4)
